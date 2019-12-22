ba6818893b6cf6afadf90c3be86a1fd14998b64f
goldbergyossi <yygold@gmail.com>
2019-12-22 09:32:47 -0500

    completed with the help of Ari D.

--- exercise1.rb
+++ exercise1.rb
@@ -1,10 +1,11 @@
-
-
-sum = 0
-    expenses = [250, 7.95, 30.95, 16.50]
-    expenses2 = [12.5, 20, 11.10]
-    expenses.each && expenses2.each do |bookkeeping|
+expenses = [250, 7.95, 30.95, 16.50]
+expenses2 = [5.95, 29, 1.99, 10.98]
+def accounting(x)
+    sum = 0
+    x.each  do |bookkeeping|
     sum += bookkeeping
-    end    
-
-    puts sum 
+    end 
+    return sum   
+end 
+    puts accounting(expenses)
+    puts accounting(expenses2)
